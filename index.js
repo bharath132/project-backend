2f4e141d9982d8f00899c91f3ae07522efc0f2c3
BARATH G <chidambaramb2@gmail.com>
2025-06-14 02:31:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const cros = require("cors");
+const sendPushNotification = require("./sendNotification.js");
+const { log } = require("console");
+const app = express();
+app.use(express.json());
+app.use(cros());
+
 app.get("/", async (req, res) => {
   const rondom = Math.floor(Math.random() * 100);
 
@@ -20,4 +28,16 @@ app.get("/", async (req, res) => {
     deviceName: "Heart Monitor",
     userValue: `${rondom}`,
   });
+});
+
+app.post("/post", (req, res) => {
+  console.log(`Received data: ${JSON.stringify(req.body)}`);
+  res.json({
+    receivedData: req.body,
+  });
+});
+// Binding the server to '0.0.0.0' allows it to accept connections from any network interface,
+// making it accessible not only from localhost but also from other devices in the same network.
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server is running on http://0.0.0.0:3000");
 });
