8483629156bf70b43c6cc0995d1a9c8e3dd99766
BARATH G <chidambaramb2@gmail.com>
2025-06-14 02:54:09 +0530

    add token to env

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 const express = require("express");
 const cros = require("cors");
 const sendPushNotification = require("./sendNotification.js");
-const { log } = require("console");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cros());
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   const rondom = Math.floor(Math.random() * 100);
-
-  if (rondom > 50) {
-    console.log("High heart rate detected:", rondom);
-    try {
-      await sendPushNotification(
-        "fRob3roCSq-GDeRd4rqv57:APA91bFZpoXyFLrXsJwAaeiM5W2ySWZLDyGm2Uk7Rl9QGUN6l9gWgfdaWHCdReyNaOGHPuV_Hdzqmg39-JNpcTBv29gE6nsXphqPNC3ARmt5Cv8o8CxDfJ0",
-        "Alert",
-        `Your heart rate is high! ${rondom} bpm`
-      );
-    } catch (error) {
-      console.error("Error while sending push notification:", error);
-    }
-  }
-
-  // Respond only after push logic completes
   res.json({
     productName: "Smart Watch",
     deviceName: "Heart Monitor",
     userValue: `${rondom}`,
   });
+  if (rondom > 50) {
+    console.log("High heart rate detected:", rondom);
+    sendPushNotification(
+      process.env.FCM_TOKEN,
+      "Alert",
+      `Your heart rate is high! ${rondom} bpm`
+    );
+  }
 });
 
 app.post("/post", (req, res) => {
