2a102aa39e735ed04b7132cd3699ceba4ef90016
BARATH G <chidambaramb2@gmail.com>
2025-06-13 10:17:42 +0530

    added post req

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cros = require("cors");
 const app = express();
-
+app.use(express.json());
 app.use(cros());
 const rondom = Math.floor(Math.random() * 1000);
 
@@ -14,7 +14,13 @@ app.get("/", (req, res) => {
 });
 });
 
-
+app.post("/post", (req, res) => {
+  
+  console.log(`Received data: ${JSON.stringify(req.body)}`);
+  res.json({
+    "receivedData": req.body,
+  });
+});
 // Binding the server to '0.0.0.0' allows it to accept connections from any network interface,
 // making it accessible not only from localhost but also from other devices in the same network.
 app.listen(3000, '0.0.0.0', () => {
