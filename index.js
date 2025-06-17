ca36736d4b5b1c9d13e31113528fa5316bee8824
BARATH G <47454425+bharath132@users.noreply.github.com>
2025-06-17 14:20:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,26 @@ app.use(cros());
 let nextRondom = 0;
 let history = {};
 app.get("/", (req, res) => {
-  if(nextRondom > 100){
+  if (nextRondom > 100) {
     nextRondom = 0; // Reset the counter if it exceeds 100
     console.log("Resetting nextRondom to 0");
   }
   const rondom = Math.floor(Math.random() * 9);
   nextRondom += rondom;
-// Store the current value in history
-if(nextRondom%50 == 0 ){  history = {
-    ...history,
-    [new Date().toISOString()]: nextRondom,
-  };}
-if (Object.keys(history).length > 50) {
-  const oldestKey = Object.keys(history)[0];
-  delete history[oldestKey];
-}
+  // Store the current value in history
+  if (nextRondom % 50 == 0) {
+    history = {
+      ...history,
+      [new Date().toISOString()]: nextRondom,
+      
+      
+    };
     console.log(history)
+  }
+  if (Object.keys(history).length > 50) {
+    const oldestKey = Object.keys(history)[0];
+    delete history[oldestKey];
+  }
   //send notification if value is high
   if (nextRondom > 50) {
     console.log("High heart rate detected:", nextRondom);
