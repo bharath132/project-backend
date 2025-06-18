0b08dbdd675b468b9b91a3be49a368bf980ce85c
BARATH G <chidambaramb2@gmail.com>
2025-06-19 02:09:00 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ const { google } = require("googleapis");
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
-const serverless = require("serverless-http");
 
+const sendPushNotification = require("./sendNotification.js");
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 let nextRondom = 0;
 let history = {};
-
-// Decode credentials.json
-
-
+let sheetData = [];
 
 const os = require("os");
-
 
 // 1. Decode credentials
 const credentialsJson = Buffer.from(
@@ -32,34 +28,56 @@ fs.writeFileSync(tempPath, credentialsJson);
 
 // 3. Auth setup
 const auth = new google.auth.GoogleAuth({
-  keyFile: tempPath, // ✅ Use the temp path
+  keyFile: tempPath, //  Use the temp path
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
 
 const spreadsheetId = "14wKbS-ddV--wrrXvx-KM0GqMEsRrUAASlWKoLdNzIMA";
+function getISTTime() {
+  const utcDate = new Date("2025-06-18T20:15:50.000Z");
+
+  return utcDate.toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
 
 // 4. Append function
 async function appendToSheet(data) {
   try {
     const authClient = await auth.getClient();
     const sheets = google.sheets({ version: "v4", auth: authClient });
-
     await sheets.spreadsheets.values.append({
       spreadsheetId,
       range: "Sheet1!A:B",
       valueInputOption: "RAW",
       resource: {
-        values: [[new Date().toISOString(), data]],
+        values: [[getISTTime(), data]],
       },
     });
-
-    console.log("✅ Appended to sheet:", data);
+    console.log(" Appended to sheet:", data);
   } catch (err) {
-    console.error("❌ Sheet error:", err.message);
+    console.error(" Sheet error:", err.message);
   }
 }
-
-app.get("/", (req, res) => {
+async function getFromSheet() {
+  try {
+    await auth.getClient();
+    const sheets = google.sheets({ version: "v4", auth });
+    const respond = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: "Sheet1!A:B",
+    });
+    sheetData = respond.data.values;
+  } catch (err) {
+    console.error("Error fetching data from sheet:", err.message);
+  }
+}
+app.get("/", async (req, res) => {
   const rondom = Math.floor(Math.random() * 9);
   nextRondom += rondom;
 
@@ -67,7 +85,15 @@ app.get("/", (req, res) => {
     nextRondom = 0; // Reset the counter if it exceeds 100
     console.log("Resetting nextRondom to 0");
   }
-  appendToSheet(nextRondom); // Append the current value to Google Sheets
+  await appendToSheet(nextRondom);
+  await getFromSheet();
+  const simplified = sheetData.filter(
+    (data) => Number(data[1]) !== 0 && Number(data[1]) % 50 === 0
+  );
+  if (simplified.length > 50) {
+    simplified.slice(-50).reverse(); // Reverse the array to show the latest values first
+  }
+  console.log("Sheet data:", simplified);
   // Store the current value in history
   if (nextRondom % 50 == 0 && nextRondom != 0) {
     history = {
@@ -98,10 +124,10 @@ app.get("/", (req, res) => {
     productName: "Urinary Bladder Monitor",
     deviceName: "Urinary Bladder Monitor",
     userValue: `${nextRondom}`,
-    history: Object.entries(history)
-      .slice()
-      .reverse()
-      .map(([time, value]) => ({ time, value })), // Reverse the history to show the latest values first
+    history: simplified.map(([time, value]) => ({
+      time,
+      value: Number(value),
+    })), // Reverse the history to show the latest values first
   });
 });
 
@@ -114,8 +140,4 @@ app.post("/post", (req, res) => {
 
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server is running on http://0.0.0.0:3000");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
 });
