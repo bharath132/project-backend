08a8d4d4fb06d76d207d7c70f1d1a47a81d814d0
BARATH G <chidambaramb2@gmail.com>
2025-06-18 16:47:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,17 +59,61 @@ async function appendToSheet(data) {
   }
 }
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   const rondom = Math.floor(Math.random() * 9);
   nextRondom += rondom;
 
-  if (nextRondom > 100) nextRondom = 0;
+  if (nextRondom > 100) {
+    nextRondom = 0; // Reset the counter if it exceeds 100
+    console.log("Resetting nextRondom to 0");
+  }
+  appendToSheet(nextRondom); // Append the current value to Google Sheets
+  // Store the current value in history
+  if (nextRondom % 50 == 0 && nextRondom != 0) {
+    history = {
+      ...history,
+      [new Date().toISOString()]: nextRondom,
+    };
+    console.log(history);
+  }
 
-  await appendToSheet(nextRondom);
+  // Limit the history to the last 50 entries
+  if (Object.keys(history).length > 50) {
+    const oldestKey = Object.keys(history)[0];
+    delete history[oldestKey];
+  }
 
+  //send notification if value is high
+  if (nextRondom > 50) {
+    console.log("High rate detected:", nextRondom);
+    sendPushNotification(
+      process.env.FCM_TOKEN,
+      "Alert",
+      `Your bag filled 50% ${rondom} `
+    );
+  }
+
+  // respond
   res.json({
-    value: nextRondom,
+    productName: "Urinary Bladder Monitor",
+    deviceName: "Urinary Bladder Monitor",
+    userValue: `${nextRondom}`,
+    history: Object.entries(history)
+      .slice()
+      .reverse()
+      .map(([time, value]) => ({ time, value })), // Reverse the history to show the latest values first
   });
+});
+
+app.post("/post", (req, res) => {
+  console.log(`Received data: ${JSON.stringify(req.body)}`);
+  res.json({
+    receivedData: req.body,
+  });
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server is running on http://0.0.0.0:3000");
 });
 
 app.listen(3000, () => {
